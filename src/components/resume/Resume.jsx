7224d20dda6a8a17c81ff0ca181fb3c4a016652a
Rutuja Rathod <127573564+18Rutuja@users.noreply.github.com>
2024-08-12 19:31:26 +0530

    Update Resume.jsx

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,7 +4,7 @@ import './Resume.css'; // CSS file for styling
 const Resume = () => {
   return (
     <div className="download-resume">
-      <a href="https://drive.google.com/file/d/1Awz2LDaig1md-IZiQ-Bi5zsafP7TXFYw/view?usp=sharing" download className="download-link">
+      <a href="https://drive.google.com/file/d/1ik-Kfet2aofWizyQQqClrdtqm6iJ3Hsd/view?usp=sharing" download className="download-link">
         Download Resume
       </a>
     </div>
